4d1e4ac2f97642d823b5219aa1503d11c083bdfe
hanuchaudhary <kushchaudharyog@gmail.com>
2025-04-13 01:15:04 +0530

    Redis pub

--- APIServer/src/index.ts
+++ APIServer/src/index.ts
@@ -1,6 +1,26 @@
 import express, { Request, Response } from "express";
 import { generateSlug } from "random-word-slugs";
 import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const LOCAL_ENVS = [
+  "AWS_REGION",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY",
+  "ECS_CLUSTER",
+  "ECS_TASK_ARN",
+  "REDIS_URL",
+  "AWS_BUCKET_NAME",
+];
+
+
+LOCAL_ENVS.forEach((env) => {
+  if (!process.env[env]) {
+    console.log("Missing ENV:", env);
+    throw new Error(`Environment variable ${env} not found`);
+  }
+});
 
 const ecsClient = new ECSClient({
   region: process.env.AWS_REGION,
@@ -10,26 +30,12 @@ const ecsClient = new ECSClient({
   },
 });
 
-const LOCAL_ENVS = [
-  "AWS_REGION",
-  "AWS_ACCESS_KEY_ID",
-  "AWS_SECRET_ACCESS_KEY",
-  "ECS_CLUSTER",
-  "ECS_TASK_ARN",
-];
-
-LOCAL_ENVS.forEach((env) => {
-  if (!process.env[env]) {
-    console.log("Missing ENV:", env);
-    throw new Error(`Environment variable ${env} not found`);
-  }
-});
-
 const app = express();
+app.use(express.json());
 
-app.get("/project", async (req: Request, res: Response) => {
+app.post("/project", async (req: Request, res: Response) => {
   try {
-    const { githubRepo = `https://github.com/hanuchaudhary/todoReact` } =
+    const { githubRepo } =
       req.body;
     const projectNameSlug = generateSlug(2);
     console.log(`Generated Slug: ${projectNameSlug}`);
@@ -66,6 +72,13 @@ app.get("/project", async (req: Request, res: Response) => {
                 name: "AWS_SECRET_ACCESS_KEY",
                 value: process.env.AWS_SECRET_ACCESS_KEY,
               },
+              {
+                name: "REDIS_URL",
+                value : process.env.REDIS_URL,
+              },{
+                name : "AWS_BUCKET_NAME",
+                value : process.env.AWS_BUCKET_NAME,
+              }
             ],
           },
         ],
@@ -76,8 +89,13 @@ app.get("/project", async (req: Request, res: Response) => {
     await ecsClient.send(command);
     console.log("Container runs successfully");
 
-    res.status(200).json({ slug: projectNameSlug });
+    res.status(200).json({
+      slug: projectNameSlug,
+      url: `http://${projectNameSlug}.localhost:8080`,
+    });
+
   } catch (error) {
+    console.log("Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
--- buildServer/script.js
+++ buildServer/script.js
@@ -3,6 +3,15 @@ const path = require("path");
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mimeTypes = require("mime-types");
+const Redis = require("ioredis");
+
+const redisClient = new Redis(process.env.REDIS_URL);
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+redisClient.on("connect", () => {
+  console.log("Connected to Redis server.");
+})
 
 const s3Client = new S3Client({
   credentials: {
@@ -18,28 +27,45 @@ if (!PROJECT_ID) {
   process.exit(1);
 }
 
+const publishLog = (log) => {
+  redisClient.publish(
+    `logs:${process.env.PROJECT_ID}`,
+    JSON.stringify({
+      log: log,
+      timestamp: new Date().toISOString(),
+    })
+  );
+};
+
 const init = () => {
-  console.log("Initializing build server...");
+  console.log("Starting build process...");
+  publishLog("Starting build process...");
   const outputDir = path.join(__dirname, "output");
 
   console.log("Executing npm install & build...");
+  publishLog("Executing npm install & build...");
   const p = exec(`cd ${outputDir} && npm install && npm run build`);
 
   p.stdout.on("data", (data) => {
     console.log("Build:", data.toString());
+    publishLog(`Build: ${data.toString()}`);
   });
 
   p.stdout.on("error", (data) => {
     console.error("error", data.toString());
+    publishLog(`Error: ${data.toString()}`);
   });
 
   p.stdout.on("close", async (code) => {
     console.log(`Build process exited with code ${code}`);
+    publishLog(`Build process exited with code ${code}`);
     const distFolderPath = path.join(outputDir, "dist");
     const distFolderContents = fs.readdirSync(distFolderPath, {
       recursive: true,
     });
-    console.log("dist folder contents", distFolderContents);
+
+    console.log("Uploading files to S3...");
+    publishLog("Uploading files to S3...");
 
     for (const file of distFolderContents) {
       const filePath = path.join(distFolderPath, file);
@@ -47,6 +73,7 @@ const init = () => {
       if (fs.lstatSync(filePath).isDirectory()) continue;
 
       console.log(`Uploading ${filePath} to S3...`);
+      publishLog(`Uploading ${filePath} to S3...`);
       const command = new PutObjectCommand({
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: `__outputs/${PROJECT_ID}/${file}`,
@@ -56,9 +83,12 @@ const init = () => {
 
       await s3Client.send(command);
       console.log(`Uploaded ${filePath} to S3`);
+      publishLog(`Uploaded ${filePath} to S3`);
     }
     console.log("Build process completed successfully.");
     console.log("All files uploaded to S3.");
+    publishLog("Build process completed successfully.");
+    publishLog("All files uploaded to S3.");
   });
 };
 
